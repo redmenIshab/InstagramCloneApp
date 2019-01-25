e30bf14c7c0a0ad8869b367ca46044c07d907b71
Ishab Acharya <ishabachary@gmail.com>
2019-01-25 16:15:02 +0545

    login layout completed

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 import { Dimensions } from "react-native";
 const Button_Width = Dimensions.get("window").width - 40;
-const Button_Height = 40;
+const Button_Height = 45;
 const Primary_Color = "#0E68CE";
 export default (configs = {
   Button_Height,
