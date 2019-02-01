61f43852b318680794bed51f4ee75109d20487ce
Ishab Acharya <ishabachary@gmail.com>
2019-02-01 16:05:08 +0545

    camera for android done

--- android/app/src/main/java/com/instagramcloneapp/MainApplication.java
+++ android/app/src/main/java/com/instagramcloneapp/MainApplication.java
@@ -4,14 +4,13 @@ import android.app.Application;
 
 import com.facebook.react.ReactApplication;
 import com.oblador.vectoricons.VectorIconsPackage;
-import com.oblador.vectoricons.VectorIconsPackage;
 import io.invertase.firebase.RNFirebasePackage;
 import io.invertase.firebase.auth.RNFirebaseAuthPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-
+import org.reactnative.camera.RNCameraPackage;
 import java.util.Arrays;
 import java.util.List;
 
@@ -26,7 +25,7 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(new MainReactPackage(), new VectorIconsPackage(), new RNFirebasePackage(),
-          new RNFirebaseAuthPackage());
+          new RNFirebaseAuthPackage(),new RNCameraPackage());
     }
 
     @Override
